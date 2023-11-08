294e7ade5b86e1d23692874cdc1284d57f6d504f
onkar <jadhavonkar333@gmail.com>
2023-11-08 16:36:33 +0530

    Assignment4

---
+++ Assignment4/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int num;
+            Console.WriteLine("enter  value");
+            num = int.Parse(Console.ReadLine());
+
+
+            if(num>0)
+            {
+                Console.WriteLine("Number is positive");
+            }
+            else if(num < 0) {
+
+                Console.WriteLine("Number is negative");
+            }
+            else
+            {
+                Console.WriteLine("Number is zero");
+            }
+
+
+            if(num%2==0)
+            {
+                Console.WriteLine("The number is even");
+            }
+            else
+            {
+                Console.WriteLine("The number is odd");
+            }
+        }
+    }
+}
